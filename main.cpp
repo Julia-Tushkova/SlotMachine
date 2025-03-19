2069182d35baff18c54af3e83993a10ff34feeac
pulewitch <92311251+Julia-Tushkova@users.noreply.github.com>
2025-03-19 14:12:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <SDL.h>
 #include <iostream>
+#include <windows.h>
 #include "Game.h"
 
-int main(int argc, char* argv[]) {
-    srand(time(0));
+// Функция WinMain
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    srand(time(0));  // Инициализация генератора случайных чисел
     Game game;
+
     if (game.init("Slot Machine", 800, 600)) {
         game.run();
     }
+
     return 0;
 }
-
